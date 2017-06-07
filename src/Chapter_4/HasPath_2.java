9bf262683852f1620e76852247f161ba6d9cf153
puhtreeseeya <puhtreeseeya@gmail.com>
2017-06-07 12:53:22 -0400

    updated Has_Path

--- src/Chapter_4/HasPath_2.java
+++ src/Chapter_4/HasPath_2.java
@@ -1,5 +1,8 @@
 package Chapter_4;
 
+/* Given a directed graph, design an algorithm to find out whether there is a route between
+two nodes. */
+
 class Neighbor {
     public int vertexNum;
     public Neighbor next;
@@ -43,28 +46,47 @@ class Graph {
         }
     }
 
-    boolean hasPath(int start, int end) {
+    boolean BFSPath(int start, int end) {
         for(Neighbor nbr = adjList[start].next; nbr != null; nbr=nbr.next) {
             if (nbr.vertexNum == end) {
                 return true;
             } else {
-                return hasPath(nbr.vertexNum, end);
+                return BFSPath(nbr.vertexNum, end);
             }
         }
         return false;
     }
+
+    boolean DFSUtil(boolean[] visited, int start, int end) {
+        visited[start] = true;
+        if(start == end) {
+            return true;
+        } else {
+            for(Neighbor nbr = adjList[start].next; nbr != null; nbr=nbr.next) {
+                if(visited[nbr.vertexNum] == false) {
+                    return DFSUtil(visited, nbr.vertexNum, end);
+                }
+            }
+        }
+        return false;
+    }
+
+    boolean DFSPath(int start, int end) {
+        boolean[] visited = new boolean[10];
+        return DFSUtil(visited, start, end);
+    }
 }
 
-public class BFS_2 {
+public class HasPath_2 {
 
     public static void main(String[] args) {
         Graph digraph = new Graph();
         digraph.addNeighbor(0,1);
         digraph.addNeighbor(0,2);
-        digraph.addNeighbor(3,0);
         digraph.addNeighbor(1,3);
+        digraph.addNeighbor(3,2);
         digraph.print();
-        System.out.println(digraph.hasPath(3,2));
-
+        System.out.println(digraph.BFSPath(1,0));
+        System.out.println(digraph.DFSPath(1,0));
     }
 }
